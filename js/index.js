488c9022617e89534f04c5b58659cd2b76cf776c
kovner chalumo <kchalumo@gmail.com>
2018-12-28 02:40:04 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
+const element = document.getElementById('main');
+
+console.log(element);
+element.addEventListener("click", function(event) {
+  element.innerHTML = "Hey YA"
+  alert('I was clicked!');
+  console.log("This callback is working.")
+})
+
