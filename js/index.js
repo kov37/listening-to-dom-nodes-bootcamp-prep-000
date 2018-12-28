13773e09d3571b01990c03d6b696ba2f2d2278c9
kovner chalumo <kchalumo@gmail.com>
2018-12-28 02:15:18 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 // you can also load the code snippets in using your browser's console
 const element = document.getElementById('main');
 
+console.log(element);
 element.addEventListener("click", function(event) {
   element.innerHTML = "Hey YA"
   alert('I was clicked!');
