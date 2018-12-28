1d155b83c96ddcf2896c1cbb3f7e1af0f059e336
kovner chalumo <kchalumo@gmail.com>
2018-12-28 02:13:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
-const element = document.getElementById("main");
+const element = document.getElementById('main');
 
 element.addEventListener("click", function(event) {
-  alert("I was clicked!");
+  alert('I was clicked!');
+  console.log("This callback is working.")
 })
 
