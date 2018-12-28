e5a68b0732d5d92b2101b54271df5e789c84bf9f
kovner chalumo <kchalumo@gmail.com>
2018-12-28 02:07:44 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 // using this file is optional
 // you can also load the code snippets in using your browser's console
-var element = document.querySelector("#")
+var element = document.querySelector("#main");
+
+main.addEventListener("click", function(event) {
+  alert("I was clicked.");
+})
+
